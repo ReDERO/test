eeb0db00307b99c352c8cfd18d564fd42b46fad1
ReDERO <iredero@gmail.com>
2015-11-14 16:42:33 +0200

    Added BookStore project

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,147 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Util;
+using System.Data.Entity;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            /*SelectList books = new SelectList(db.Books, "Author", "Name");
+            ViewBag.Books = books;*/
+            return View();
+        }
+
+        [HttpPost]
+        public string Index(string[] countries)
+        {
+            string result = "";
+            foreach (string c in countries)
+            {
+                result += c;
+                result += ";";
+            }
+            return "Вы выбрали: " + result;
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет мир!</h2>");
+        }
+
+        public ActionResult GetImage(string path)
+        {
+            string filepath = "../"+path;
+            return new ImageResult(filepath);
+        }
+
+        public ActionResult Partial()
+        {
+            ViewBag.Message = "Это частичное представление.";
+            return PartialView();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult BookView(int id)
+        {
+            return View(db.Books.First(book => book.Id == id));
+        }
+
+        [HttpGet]
+        public ActionResult EditBook(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book book = db.Books.Find(id);
+            if (book != null)
+            {
+                return View(book);
+            }
+            return HttpNotFound();
+        }
+
+        [HttpPost]
+        public ActionResult EditBook(Book book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            db.Books.Remove(b);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
